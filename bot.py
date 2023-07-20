4ec669fe53f9a98ce4830c98eb42c5ed5199aac3
kaoekb <58954169+kaoekb@users.noreply.github.com>
2023-07-20 17:10:25 +0500

    Update delete

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 from config import Token_MDB, Token_tg 
 from pymongo import MongoClient
+from telebot import types
 
 bot = telebot.TeleBot(Token_tg)
 
@@ -31,6 +32,9 @@ class DataBase:
 	def set_user(self, chat_id, update):
 		self.login.update_one({"chat_id": chat_id}, {"$set": update})
 
+	def delete_user(self, user_id):
+		self.login.delete_one({"user_id": user_id})
+
 db = DataBase()
 
 def find_login(login): # Проверка, есть ли такой логин в БД
@@ -50,13 +54,12 @@ def handle_start(message):
     if db.login.find_one({"user_id": user_id}) is not None:
         # Если пользователь уже есть в базе данных, переходим в функцию callback
             bot.register_next_step_handler(message, callback)
-            bot.send_message(message.chat.id, 'Введи школьный или телеграм ник (без @) интересующего тебя пира.')
+            bot.send_message(message.chat.id, 'Введи школьный или телеграм ник интересующего тебя пира.')
 
     else:
         # Если пользователь новый, отправляем сообщение с просьбой ввести школьный ник
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}, введи свой школьный ник')
         bot.register_next_step_handler(message, hi)
-
 
 def hi(message):
     # Получаем логин пользователя в нижнем регистре
@@ -71,17 +74,59 @@ def hi(message):
     else:
         # Если логин есть, добавляем логины в базу данных
         db.login.insert_one({"login_school": login_school, "login_tg": login_tg, "user_id": user_id})
-        bot.send_message(message.chat.id, 'Введи школьный или телеграм ник (без @) интересующего тебя пира.')
+        bot.send_message(message.chat.id, 'Введи школьный или телеграм ник интересующего тебя пира.')
     bot.register_next_step_handler(message, callback)
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    # Отправляем сообщение с помощью по использованию бота
+    help_text = "Привет! Я бот для поиска пользователей по школьному или телеграм нику.\n\n" \
+                "Чтобы начать, введи команду /start и следуй инструкциям.\n\n" \
+                "Чтобы удалить логин, введи команду /delete. В дальнейшем по команде /start ты сможешь создать новый логин.\n\n" \
+                "Если у тебя возникли вопросы или проблемы - обратись к администратору @kaoekb."
+    bot.send_message(message.chat.id, help_text)
+
+@bot.message_handler(commands=['delete'])
+def handle_delete(message):
+    # Получаем id пользователя
+    user_id = message.from_user.id
+    # Проверяем, есть ли пользователь в базе данных
+    if db.login.find_one({"user_id": user_id}) is not None:
+        # Если пользователь есть в базе данных, отправляем сообщение с подтверждением удаления
+        confirm_message = "Ты точно хочешь удалить свой логин?"
+        confirm_keyboard = types.InlineKeyboardMarkup()
+        yes_button = types.InlineKeyboardButton("Да", callback_data='confirm_yes')
+        no_button = types.InlineKeyboardButton("Нет", callback_data='confirm_no')
+        confirm_keyboard.row(yes_button, no_button)
+        bot.send_message(message.chat.id, confirm_message, reply_markup=confirm_keyboard)
+    else:
+        bot.send_message(message.chat.id, 'Ты ещё не зарегистрирован.')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_confirmation(call):
+    if call.data == 'confirm_yes':
+        # Получаем id пользователя
+        user_id = call.from_user.id
+        # Удаляем запись пользователя из базы данных
+        db.delete_user(user_id)
+        bot.send_message(call.message.chat.id, 'Твоя запись удалена из базы данных.')
+    elif call.data == 'confirm_no':
+        bot.send_message(call.message.chat.id, 'Отменено.')
 
 
 @bot.message_handler(content_types=['text'])
 def callback(message):
     login = message.text.lower()
+        # Удаляет символ "@", если он является первым символом текста сообщения        
+    if login.startswith('@'):
+        login = login[1:]
+    
     result = find_login(login)
     if result is None:
         bot.send_message(message.chat.id, "Логин не найден")
-        bot.send_message(message.chat.id, 'Введи школьный или телеграм ник (без @) интересующего тебя пира.')
+        bot.send_message(message.chat.id, 'Введи школьный или телеграм ник интересующего тебя пира.')
 
     else:
         text = f"Login school: {result[0].capitalize()}, login tg: @{result[1].capitalize()}"
